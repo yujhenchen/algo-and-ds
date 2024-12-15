19ceac13a420d8cf78207a093932540c9830994d
yujhenchen <yujhen.chen93@gmail.com>
2024-12-15 16:50:28 +0800

    feat: implement countUniqueValues and unit tests

--- src/multiplePointers/index.ts
+++ src/multiplePointers/index.ts
@@ -28,3 +28,21 @@ export function sumZero(arr: Array<number>): Array<number> {
     }
     return [];
 }
+
+// pass a sorted array, and return the count of unique values
+export function countUniqueValues(arr: Array<number>): number {
+    // return array length if the arr length < 2
+    // loop the array, if previous !== current, count++, previous = current, else  current ++
+    if (arr.length < 2) {
+        return arr.length;
+    }
+    let count = 1;
+    let prev = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (prev !== arr[i]) {
+            count++;
+            prev = arr[i];
+        }
+    }
+    return count;
+}
--- test/multiplePointers.test.ts
+++ test/multiplePointers.test.ts
@@ -1,5 +1,6 @@
-import { sumZero } from "../src/multiplePointers";
+import { countUniqueValues, sumZero } from "../src/multiplePointers";
 
+// test sumZero
 test("sumZero: given array [], should be []", () => {
     expect(sumZero([])).toEqual([]);
 });
@@ -18,4 +19,34 @@ test("sumZero: given array [1, 2, 3], should be []", () => {
 
 test("sumZero: given array [-2, 0, 1, 3], should be []", () => {
     expect(sumZero([-2, 0, 1, 3])).toEqual([]);
+});
+
+// test countUniqueValues
+test("countUniqueValues: given array length less than 2, should return array length", () => {
+    expect(countUniqueValues([1])).toEqual(1);
+    expect(countUniqueValues([])).toEqual(0);
+});
+
+test("countUniqueValues: given array with unique values, should return array length", () => {
+    expect(countUniqueValues([1, 2, 3, 4, 5])).toEqual(5);
+});
+
+test("countUniqueValues: given array with duplicate values, should return unique count", () => {
+    expect(countUniqueValues([1, 1, 1, 1, 1, 2])).toEqual(2);
+});
+
+test("countUniqueValues: given array with duplicate values at the beginning, should return unique count", () => {
+    expect(countUniqueValues([1, 1, 2, 3, 4, 4, 5])).toEqual(5);
+});
+
+test("countUniqueValues: given array with duplicate values at the end, should return unique count", () => {
+    expect(countUniqueValues([1, 2, 3, 4, 5, 5, 5])).toEqual(5);
+});
+
+test("countUniqueValues: given array with a single unique value, should return 1", () => {
+    expect(countUniqueValues([1, 1, 1, 1, 1])).toEqual(1);
+});
+
+test("countUniqueValues: given an empty array, should return 0", () => {
+    expect(countUniqueValues([])).toEqual(0);
 });
